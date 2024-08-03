d756affab60311fe522970b75fb8e781449a0167
Cawdroch <77812106+Cawdroch@users.noreply.github.com>
2024-08-03 21:06:50 +0100

    Completed Testing

--- __tests__/userController.test.js
+++ __tests__/userController.test.js
@@ -1,37 +1,34 @@
-const { registerUser } = require("../controllers/userController");
+const { registerUser } = require('../controllers/userController');
 
-// Mock user model functions
+// Mock User model functions
+jest.mock('../models/userModel', () => {
+    // Mock User model
+    const mockUser = {
+        _id: 'user-id',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+    };
 
-jest.mock("../models/userModel", () => {
-  // Mock user model
-  const mockUser = {
-    _id: "user-id",
-    name: "John Doe",
-    email: "johndoe@example.com",
-  };
-  return {
-    findOne: jest.fn().mockResolvedValue(null),
-    create: jest.fn().mockResolvedValue(mockUser),
-  };
+    return {
+        findOne: jest.fn().mockResolvedValue(null),
+        create: jest.fn().mockResolvedValue(mockUser),
+    };
 });
-
 // Mock JWT and bcrypt
-
-jest.mock("jsonwebtoken", () => {
-  sign: jest.fn().mockReturnValue("mock-token");
-});
-const bcrypt = require("bcryptjs");
-bcrypt.genSalt = jest.fn().mockResolvedValue("mock-salt");
-bcrypt.hash = jest.fn().mockResolvedValue("mock-hashed-password");
+jest.mock('jsonwebtoken', () => ({
+    sign: jest.fn().mockReturnValue('mock-token'),
+}));
+const bcrypt = require('bcryptjs');
+bcrypt.genSalt = jest.fn().mockResolvedValue('mock-salt');
+bcrypt.hash = jest.fn().mockResolvedValue('mock-hashed-password');
 
 // Test
-
-test("should register a new user", async () => {
+test('should register a new user', async () => {
   const req = {
     body: {
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "password",
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
     },
   };
 
@@ -42,4 +39,22 @@ test("should register a new user", async () => {
 
   await registerUser(req, res);
   expect(res.status).toHaveBeenCalledWith(201);
+});
+
+test('should return a 400 error if any field is missing', async () => {
+    const req = {
+      body: {
+        name: 'John Doe',
+        email: '', // Missing email field intentionally
+        password: 'password',
+      },
+    };
+  
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  
+    await expect(registerUser(req, res)).rejects.toThrow('All fields are mandatory');
+    expect(res.status).toHaveBeenCalledWith(400);
 });
